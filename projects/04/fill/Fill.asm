91f2a91449bd1d63024c1f9b2eadb3c62be5d822
mytinylabo <mytinylabo@gmail.com>
2019-07-27 09:33:54 +0900

    Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,39 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(CHECKKEY)
+        @KBD
+        D=M
+        @FILL_BLACK
+        D;JGT
+(FILL_WHITE)
+        @8192 // (512/16)*256
+        D=A
+(WHITE_STEP)
+        D=D-1
+        // M[SCREEN+D] = 0b0000000000000000
+        @SCREEN
+        A=A+D
+        M=0
+        @WHITE_STEP
+        D;JGT
+        @CHECKKEY
+        0;JMP
+(FILL_BLACK)
+        @8192 // (512/16)*256
+        D=A
+(BLACK_STEP)
+        D=D-1
+        // M[SCREEN+D] = 0b1111111111111111
+        @SCREEN
+        A=A+D
+        M=-1
+        @BLACK_STEP
+        D;JGT
+        @CHECKKEY
+        0;JMP
